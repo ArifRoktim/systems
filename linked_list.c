6dc43df2cdff6cc0ef99439dec6c55b71201c44c
Arif Roktim <aroktim@stuy.edu>
2017-10-13 12:28:07 -0400

    Did insert_front and print_list

--- linked_list.c
+++ linked_list.c
@@ -10,8 +10,24 @@ struct node * insert_front( struct node *node, char *text ){
     struct node *front = malloc( sizeof(struct node) );
     front->text = text;
     front->next = node;
+    return front;
+}
+
+void print_list( struct node *list ){
+    while( list ){
+        printf("%s->", list->text );
+        list = list->next;
+    }
+    printf("\n");
 }
 
 int main(){
-    struct node *llist;
+    struct node *llist = 0;
+    llist = insert_front( llist, "is bob" );
+    llist = insert_front( llist, "bobbins" );
+    llist = insert_front( llist, "bob" );
+
+    print_list( llist );
+
+    return 0;
 }
