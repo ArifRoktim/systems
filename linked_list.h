90e5aea26a1436b3135dfe2c3a21b68cbeacbe89
Arif Roktim <aroktim@stuy.edu>
2017-10-18 21:18:06 -0400

    Update header file

--- linked_list.h
+++ linked_list.h
@@ -2,6 +2,7 @@ struct node * insert_front( struct node *node, char *artist, char *name );
 void print_list( struct node *list );
 struct node * free_list( struct node *list );
 struct node * find( struct node *list, char *artist, char * name );
-struct node * get_index( struct node * list, int index);
+struct node * get_node( struct node * list, int index);
 struct node * insert( struct node * list, int index, char *artist, char *name);
+struct node * insert_at_node( struct node * node, char *artist, char *name );
 struct node * insert_order( struct node *list, char *artist, char *name);
