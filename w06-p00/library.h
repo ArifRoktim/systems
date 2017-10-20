58bee244e5b191475fb5864fa55e23544776efa5
Arif Roktim <arifrroktim@gmail.com>
2017-10-20 10:20:00 -0400

    Fix header file

--- w06-p00/library.h
+++ w06-p00/library.h
@@ -1,2 +1,1 @@
-struct node * create_library();
-struct node * add_song(struct node * library, char *artist, char *name);
+struct node * add_song(struct node *library[26], char *artist, char *name);
