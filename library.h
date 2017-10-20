b1ba4a746b4028f478ca4c01c387a838e2711c73
aroktim@stuy.edu <aroktim@stuy.edu>
2017-10-20 13:22:46 -0400

    Fix header file

--- library.h
+++ library.h
@@ -4,3 +4,4 @@ void print_library( struct node *library[26] );
 void print_letter( struct node *library[26], char letter);
 struct node * find_artist_library( struct node *library[26], char *artist);
 void print_artist( struct node *library[26], char *artist);
+struct node * find_song( struct node *library[26], char *artist, char *name );
