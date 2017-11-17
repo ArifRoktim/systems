ce6bca37fd8dd6d8241121517bc72c47bfacd2fb
Arif Roktim <arifrroktim@gmail.com>
2017-11-16 20:07:52 -0500

    Made code less redundant

--- w12/w12.c
+++ w12/w12.c
@@ -8,32 +8,25 @@ int main(){
   printf("Pre fork!\n");
 
   int f = fork();
+  int f1 = 0;
   int status;
 
   if(f){
     int f1 = fork();
-    if(f1){
+    if( f1 ){
       int pid = wait(&status);
       printf("Child with pid %d slept for %d seconds\n", pid, WEXITSTATUS(status));
       printf("Parent done.\n");
       exit(0);
     }
-    if( f1 == 0 ){
-      printf("I'm a child! My pid is: %d\n", getpid());
-      srand(time(NULL));
-      int randint = rand() % 16 + 5; // return rand int in range [5,20]
-      sleep(randint);
-      //printf("Slept for %d seconds.\n",randint);
-      return randint;
-    }
-  } 
-  if( f == 0 ){
+  }
+  if( f == 0 || f1 == 0 ){
     // Child process
     printf("I'm a child! My pid is: %d\n", getpid());
-    srand(time(NULL));
+    srand(time(NULL) + getpid());
     int randint = rand() % 16 + 5; // return rand int in range [5,20]
+    //printf("Sleeping for %d seconds.\n",randint);
     sleep(randint);
-    //printf("Slept for %d seconds.\n",randint);
     return randint;
   }
 }
