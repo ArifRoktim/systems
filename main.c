8cd9d9abbb5afe9e13b1855c95450a2d1d54b8cc
aroktim@stuy.edu <aroktim@stuy.edu>
2017-11-22 12:01:00 -0500

    Add main

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include "w11.h"
+#include "builtin.h"
 
 int main( int argc, int *argv[] ){
 
   int loop = 1;
   while(loop){
     char input[100];
+    printf("$ ");
     fgets(input, sizeof(input), stdin);
     printf("You entered: %s", input);
+    char **args = parse_args(input);
+    //print_str_arr(args);
+    // fork off and exec the command
+    fork_and_exec( args[0], args );
   }
 }
