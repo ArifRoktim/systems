fa738fec4ad1aeb80fb3e72349239bd8b6b321a4
Arif Roktim <aroktim@stuy.edu>
2017-11-25 21:27:48 -0500

    Cleanup main function

--- builtin.c
+++ builtin.c
@@ -3,10 +3,41 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include "builtin.h"
+#include "parse.h"
 
 void cd(char *dir){
   int status = chdir(dir);
   if(status){
     printf("shell: cd: %s: %s\n", dir, strerror(errno));
   }
+}
+
+// Do pre-exit stuff here
+void safely_exit(){
+  // TO BE IMPLEMENTED LATER
+  exit(0);
+}
+
+
+void print_prompt(){
+  // Get user's cwd
+  char cwd[256];
+  getcwd(cwd, sizeof(cwd));
+  // Replace the home directory path with '~'
+
+  
+  printf("%s $ ", cwd);
+}
+
+char * get_input(){
+  char raw_input[100];
+  if( fgets(raw_input, sizeof(raw_input), stdin) == NULL ){
+    safely_exit();
+  }
+  strip_newline(raw_input);
+  char *input = strip_spaces(raw_input);
+  //printf("Raw: %s\nInput: %s\n", raw_input, input);
+  //printf("Input 0: %s\n", input);
+  return input;
 }
--- main.c
+++ main.c
@@ -3,24 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include "parse.h"
-
-void print_prompt(){
-  char cwd[256];
-  getcwd(cwd, sizeof(cwd));
-  printf("%s $ ", cwd);
-}
+#include "builtin.h"
 
 int main( int argc, char *argv[] ){
 
   int main_loop = 1;
   while(main_loop){
     print_prompt();
-    char raw_input[100];
-    fgets(raw_input, sizeof(raw_input), stdin);
-    strip_newline(raw_input);
-    char *input = strip_spaces(raw_input);
-    //printf("Raw: %s\nInput: %s\n", raw_input, input);
-    //printf("Input 0: %s\n", input);
+    char *input = get_input();
 
     read_and_exec(input);
     free(input);
