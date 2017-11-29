f343064b7e1d7240276aeefdd8577f973a5ca35f
Dregassa <dregassa@stuy.edu>
2017-11-29 11:36:22 -0500

    parse.c

--- p01/new_parse.c
+++ p01/new_parse.c
@@ -23,11 +23,38 @@ char ** parse_args(char * s1, char * delim){
 	//maybe put a realloc here
 	return ret;
 }
+void print_bytes(char * str, int size){
+	int i = 0;
+	printf("\n[");
+	char to_print;
+	for (;i<size;i++){
+		//printf("%i", i);
+		to_print = str[i];
+		if (to_print == 0){
+			printf("/0");
+		}
+		else if (to_print == ' '){
+			printf("_");
+		}
+		else{
+			printf("%c", to_print);
+		}
+	}
+	printf("]\n");
+}
+
 int main(){
 	printf("BUFSIZ = %d\n", BUFSIZ);
 	//char * line = "ls -a -l";
-	char line[100] = "  ls  -a -l";
+	char line[100] = "ls > foo.txt";
 	char * m = line;
-	char ** test = parse_args(m, " ");
-	execvp(test[0], test);
+	char ** test = parse_args(m, ">");
+	
+	//printf("%s", line);
+	int i = 0;
+	for (;i<2;i++){
+		printf("%s", test[i]);
+	}
+//	print_bytes(line, 100);
+	printf('\n');
 }
