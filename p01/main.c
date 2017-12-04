69a8fd3f2fbc91fb61045a9da3488270e663327c
Arif Roktim <arifrroktim@gmail.com>
2017-12-04 07:30:29 -0500

    Fix rainbow mode and fflush stdout for prompt

--- p01/main.c
+++ p01/main.c
@@ -13,29 +13,7 @@
 int loop = 1;
 char *vars[256][2];
 
-void rainbow(){
-  mkfifo("/tmp/shell", 0600);
-  int f = fork();
-  if(f){
-    int file = open("/tmp/shell", O_RDONLY);
-    while(loop){
-      int f1 = fork();
-      if(f1){
-        wait(0);
-      } else {
-        dup2(file, STDIN_FILENO);
-        execlp("lolcat", "lolcat");
-      }
-    }
-  } else {
-    int file = open("/tmp/shell", O_WRONLY);
-    dup2(file, STDOUT_FILENO);
-  }
-
-}
-
 int main( int argc, char *argv[] ){
-  
   
   int i = 1;
   int bool_rainbow = 0;
@@ -52,18 +30,19 @@ int main( int argc, char *argv[] ){
   // Run initialization stuff
   init();
 
-	while(loop){
-		print_prompt();
-		char *input = get_input();
+  while(loop){
+    print_prompt();
+    fflush(stdout);
+    char *input = get_input();
 
-		if( input ){
-			read_and_exec(input);
-			free(input);
-		}
-	}
+    if( input ){
+      read_and_exec(input);
+      free(input);
+    }
+  }
 
-	// free stuff (and later save history)
-	cleanup();
+  // free stuff (and later save history)
+  cleanup();
 
-	return 0;
+  return 0;
 }
--- p01/misc.h
+++ p01/misc.h
@@ -3,3 +3,7 @@
 void init();
 //Frees the global variable array responsible for holding shell variables
 void cleanup();
+//Makes a pipe and forks. Parent opens pipe and redirects stdout to pipe.
+//Child opens pipe and redirects stdin to pipe. Forks and execs lolcat
+//Result is that stdout output is rainbow
+void rainbow();
